ac090177c17444f7bb08c9ee195b9b3e8d7b152d
Sandra Trinh <77083798+snowb-x@users.noreply.github.com>
2022-09-03 02:43:31 +1000

    gamemanager template script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    //Singleton
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("There is no GameManger instance in the scene");
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            //destroy duplicates
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+}
