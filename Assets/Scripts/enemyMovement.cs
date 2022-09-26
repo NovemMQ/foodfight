6d8835831cb4742f287f6e482e9fb1e52801db1a
Lachlan <lachlanterrey@yahoo.com>
2022-09-26 20:52:39 +1000

    feat: init enemy scripts and components

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,16 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
---
+++ Assets/Scripts/enemyMovementManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<GameObject> objects;
+
+    public GameObject waypointList;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
