60cd4393f021f798e59025fb185a2cb71ce47fb1
snowb-x <s.andra_96@hotmail.com>
2022-09-26 22:51:10 +1000

    enemy can follow a list of way points

--- Assets/Scripts/enemyMovement.cs
+++ Assets/Scripts/enemyMovement.cs
@@ -5,12 +5,21 @@ using UnityEngine.AI;
 public class enemyMovement : MonoBehaviour
 {
 
-    public Transform goal;
+    //public Transform goal;
+    public bool moving = false;
+    public NavMeshAgent agent;
 
     void Start()
+    { 
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = goal.position;
+    }
+
+    void Update()
     {
-        
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        if((gameObject.transform.position.x == agent.destination.x) && (gameObject.transform.position.z == agent.destination.z))
+        {
+            moving = false;
+        }
     }
 }
--- Assets/Scripts/enemyMovementManager.cs
+++ Assets/Scripts/enemyMovementManager.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class enemyMovementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
 
-    public List<GameObject> objects;
-
-    public GameObject waypointList;
+    public GameObject enemyListOb;
+    public enemyMovement[] enemyList;
+    public Transform[] waypointList;
+    private int current = 0;
 
     void Start()
     {
-        
+        waypointList = gameObject.GetComponentsInChildren<Transform>();
+        enemyList = enemyListOb.GetComponentsInChildren<enemyMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (enemyMovement enemy in enemyList) {
+            if (enemy.moving == false)
+            {
+                current++;
+                if (current >= waypointList.Length)
+                {
+                    current = 1;
+                }
+                enemy.agent.destination = waypointList[current].position;
+                enemy.moving = true;
+            } }
     }
+
 }
