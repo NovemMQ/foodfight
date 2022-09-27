7f5fbc18f62a87e336a9959c5306502d657bd531
scara2016 <69334852+scara2016@users.noreply.github.com>
2022-09-27 11:08:56 +1000

    TestLauncher

---
+++ Assets/Scripts/LauncherTest.cs
@@ -0,0 +1,42 @@
+﻿using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject point2;
+    [SerializeField]
+    private GameObject foodPrefab;
+    private Transform leftHandTransform;
+    private Transform rightHandTransform;
+
+    private void Awake()
+    {
+        leftHandTransform = UserInputs.Instance.LeftHand.Transform;
+        rightHandTransform = UserInputs.Instance.RightHand.Transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject food = Instantiate(foodPrefab);
+            food.transform.position = transform.position;
+            Rigidbody foodRB = food.GetComponent<Rigidbody>();
+            foodRB.AddForce(Vector3.Normalize(point2.transform.position-transform.position)*10, ForceMode.Impulse);
+        }
+        if (VRDevice.Device.SecondaryInputDevice.GetButtonDown(VRButton.One))
+        {
+            GameObject food = Instantiate(foodPrefab);
+            food.transform.position = leftHandTransform.position;
+            Rigidbody foodRB = food.GetComponent<Rigidbody>();
+            foodRB.AddForce(Vector3.Normalize(rightHandTransform.position - leftHandTransform.position) * 10, ForceMode.Impulse);
+        }
+
+
+    }
+}
--- Assets/Scripts/UserInputs.cs
+++ Assets/Scripts/UserInputs.cs
@@ -1,4 +1,34 @@
-public class UserInputs
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Avatars;
+using UnityEngine;
+
+public class UserInputs : MonoBehaviour
 {
+    private static UserInputs instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+
+    [SerializeField]
+    private VRAvatarHand rightHand;
+    [SerializeField]
+    private VRAvatarHand leftHand;
+    public VRAvatarHand LeftHand { get => leftHand;/* set => leftHand = value; */}
+    public VRAvatarHand RightHand { get => rightHand;/* set => rightHand = value;*/ }
+    public static UserInputs Instance { get => instance;}
+    
+    
+
     //VR controller inputs here
+
 }
