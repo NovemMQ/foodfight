a89df64f423b16a343e9d2c7f9524d6c2d7c9ff8
scara2016 <guriqbalsingh.majhail@students.mq.edu.au>
2022-09-28 18:16:41 +1000

    Im a dumbass

--- Assets/LauncherGrabber.cs
+++ Assets/LauncherGrabber.cs
@@ -10,6 +10,7 @@ public class LauncherGrabber : MonoBehaviour
     private bool isHeld=false;
     private void OnTriggerEnter(Collider other)
     {
+        
         Debug.Log("TriggerActivated");
         VRAvatarHand hand = other.gameObject.GetComponent<VRAvatarHand>();
         if (!isHeld)
