092d31609566a4578ec7e37d007c9dead2f7cbb8
scara2016 <69334852+scara2016@users.noreply.github.com>
2022-09-29 10:43:01 +1000

    DieDieDie

---
+++ Assets/DestroyerEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerEnemy : MonoBehaviour
+{
+    Destroyer typeCheck;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        typeCheck = other.gameObject.GetComponent<Destroyer>();
+        if (typeCheck != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        typeCheck = collision.gameObject.GetComponent<Destroyer>();
+        if (typeCheck != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
