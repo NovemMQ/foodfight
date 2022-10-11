896ddcbb2cb1d0201450aed58afe55d5acc26d03
Jimmy Ouk <jimmy.ouk@students.mq.edu.au>
2022-10-11 13:31:26 +1100

    Went back to using Blend Trees
    I actually underestimated their usefulness, so did those instead.

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -118,54 +118,39 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void UpdateAnimator(Vector3 move)
 		{
-			// // update the animator parameters
-			// m_Animator.SetFloat("Run", m_ForwardAmount, 0.1f, Time.deltaTime);
+			// update the animator parameters
+			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
 			// m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
 			// m_Animator.SetBool("Crouch", m_Crouching);
-			// m_Animator.SetBool("Idle", m_IsGrounded);
-			// if (!m_IsGrounded)
-			// {
-			// 	m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
-			// }
-
-			// // calculate which leg is behind, so as to leave that leg trailing in the jump animation
-			// // (This code is reliant on the specific run cycle offset in our animations,
-			// // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-			// float runCycle =
-			// 	Mathf.Repeat(
-			// 		m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
-			// float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
-			// if (m_IsGrounded)
-			// {
-			// 	m_Animator.SetFloat("JumpLeg", jumpLeg);
-			// }
-
-			// // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
-			// // which affects the movement speed because of the root motion.
-			// if (m_IsGrounded && move.magnitude > 0)
-			// {
-			// 	m_Animator.speed = m_AnimSpeedMultiplier;
-			// }
-			// else
-			// {
-			// 	// don't use that while airborne
-			// 	m_Animator.speed = 1;
-			// }
-
-			//Running
-			if (m_ForwardAmount > 0.2)
+			m_Animator.SetBool("Idle", m_IsGrounded);
+			if (!m_IsGrounded)
 			{
-				m_Animator.SetBool("Idle", false);
-				m_Animator.SetBool("Run", true);
-			}
-			else if (m_ForwardAmount < 0.2)
-			{
-				m_Animator.SetBool("Idle", true);
-				m_Animator.SetBool("Run", false);
+				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
 			}
 
+			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
+			// (This code is reliant on the specific run cycle offset in our animations,
+			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
+			float runCycle =
+				Mathf.Repeat(
+					m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+			float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
+			if (m_IsGrounded)
+			{
+				m_Animator.SetFloat("JumpLeg", jumpLeg);
+			}
 
-
+			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+			// which affects the movement speed because of the root motion.
+			if (m_IsGrounded && move.magnitude > 0)
+			{
+				m_Animator.speed = m_AnimSpeedMultiplier;
+			}
+			else
+			{
+				// don't use that while airborne
+				m_Animator.speed = 1;
+			}
 		}
 
 
