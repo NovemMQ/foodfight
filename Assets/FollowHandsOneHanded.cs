6f31d75f941f4d1f79c802f436935d1e777fa101
scara2016 <guriqbalsingh.majhail@students.mq.edu.au>
2022-10-12 14:40:54 +1100

    Test

---
+++ Assets/FollowHandsOneHanded.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHandsOneHanded : MonoBehaviour
+{
+    [SerializeField]
+    Transform HandHook;
+
+    private void FixedUpdate()
+    {
+        SetPos();
+    }
+
+    private void SetPos()
+    {
+        transform.position = HandHook.position;
+        transform.SetParent(HandHook);
+    }
+
+}
