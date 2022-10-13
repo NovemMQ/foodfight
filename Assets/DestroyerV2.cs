8bb32011d47f7811787cbabe1dffe13f4cb89173
scara2016 <69334852+scara2016@users.noreply.github.com>
2022-10-13 13:58:35 +1100

    time for pain

---
+++ Assets/DestroyerV2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerV2 : MonoBehaviour
+{
+    [SerializeField]
+    private float deathTimer = 3f;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = deathTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("penis");
+ //       GameObject spFX = Instantiate(sparklePFX);
+ //       spFX.transform.position = transform.position;
+ //       ParticleSystem spFXPFX = spFX.GetComponent<ParticleSystem>();
+ //       spFXPFX.Play();
+
+        Destroy(this.gameObject);
+    }
+}
