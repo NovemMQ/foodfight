49f13452d665aa0220573b7e996af44ab6fd2c47
snowb-x <s.andra_96@hotmail.com>
2022-10-16 04:34:59 +1100

    added player got hit damage score

---
+++ Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    private ScoreKeeper ScoreManager;
+    private void Start()
+    {
+        ScoreManager = FindObjectOfType<ScoreKeeper>();
+    }
+    //die when hit by food
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TagManager.CompareTags(other.gameObject, "enemyFood"))
+        {
+            ScoreManager.addPlayerGotHit();
+        }
+    }
+}
