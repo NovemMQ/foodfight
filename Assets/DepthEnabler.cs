cb5a766f0ec8813e70a48db98235239072406686
scara2016 <guriqbalsingh.majhail@students.mq.edu.au>
2022-10-26 23:30:18 +1100

    BigBrain

---
+++ Assets/DepthEnabler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class DepthEnabler : MonoBehaviour
+{
+    public Camera[] cameras;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].depthTextureMode = DepthTextureMode.Depth;
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
