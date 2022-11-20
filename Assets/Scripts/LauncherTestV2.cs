f51497f8c17c1693dd4d13d9f51d5839da048b21
Jimmy Ouk <jimmy.ouk@students.mq.edu.au>
2022-11-20 21:41:51 +1100

    The next food object shows within the barrel
    BROKEN.

--- Assets/Scripts/LauncherTestV2.cs
+++ Assets/Scripts/LauncherTestV2.cs
@@ -12,7 +12,7 @@ public class LauncherTestV2 : MonoBehaviour
         primary,
         secondary
     }
-    
+
     private AudioSource launchSound;
     [SerializeField] private float shootHapticDuration;
     [SerializeField] private GameManager gameMnager;
@@ -22,9 +22,11 @@ public class LauncherTestV2 : MonoBehaviour
     private GameObject pivotPoint;
     [SerializeField]
     private GameObject launchPoint;
+    [SerializeField]
+    private GameObject barrelPoint;
     public List<GameObject> foodPrefab;
     [SerializeField]
-    [Range(2,40)]
+    [Range(2, 40)]
     private float foodVelocity;
     [SerializeField]
     [Range(2, 20)]
@@ -41,6 +43,7 @@ public class LauncherTestV2 : MonoBehaviour
     private bool hapticFlag = false;
     float timerwait = 0.3f;
     float reloadtimer = 0.05f;
+    private GameObject barrelFood;
     private void Awake()
     {
         currentAmmo = maxAmmoCount;
@@ -74,10 +77,11 @@ public class LauncherTestV2 : MonoBehaviour
                 currentAmmo++;
             }
         }
-        
-        if (timer <= 0&&currentAmmo>0)
+
+        if (timer <= 0 && currentAmmo > 0)
         {
-            switch (gunSide) {
+            switch (gunSide)
+            {
                 case GunSide.primary:
                     if (hapticTimer <= 0)
                     {
@@ -90,7 +94,7 @@ public class LauncherTestV2 : MonoBehaviour
                         Shoot();
                         OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
                         OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
-                        
+
                     }
                     break;
                 case GunSide.secondary:
@@ -109,11 +113,14 @@ public class LauncherTestV2 : MonoBehaviour
                     break;
             }
         }
+
+        barrelFood.transform.position = barrelPoint.transform.position;
+        barrelFood.transform.rotation = barrelPoint.transform.rotation;
     }
     public void Shoot()
     {
         hapticFlag = true;
-        timer = 0.2f;    
+        timer = 0.2f;
         launchSound.Play();
         timerwait = 1f;
         GameObject food = FoodPoolManager.getRandomFoodPlayer();
@@ -124,6 +131,29 @@ public class LauncherTestV2 : MonoBehaviour
         foodRB.AddTorque(new Vector3(Random.Range(0, rotatePower), Random.Range(0, rotatePower), Random.Range(0, rotatePower)), ForceMode.Impulse);
         currentAmmo--;
         scoreManager.addFoodThrown();
+
+        barrelFood = food;
+        ChangeBarrelFood(barrelFood);
+    }
+
+    // Changes food object that is inside the object.
+    private void ChangeBarrelFood(GameObject barrelFood)
+    {   
+        //Removes all components from food object, but Transform and Mesh Renderer
+        foreach (var comp in barrelFood.GetComponents<Component>())
+        {
+            if (!(comp is Transform))
+            {
+                if (!(comp is MeshFilter))
+                {
+                    if (!(comp is MeshRenderer))
+                    {
+                        Destroy(comp);
+                    }
+                }
+            }
+        }
+        barrelFood.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
     }
 }
 
